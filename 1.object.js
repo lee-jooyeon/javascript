98688cd1a7b12eba67f542e91f788d381b31d026
이주연 <aspyn@plgrim.com>
2022-07-29 11:27:45 +0900

    object

---
+++ 1.object.js
@@ -0,0 +1,25 @@
+// Object literal {key: value} value -> 원시값, 객체 (함수)
+// new Object()
+// object.create();
+// key - 문자, 숫자, 문자열, 심볼
+// value - 원시값, 객체 (함수)
+
+let apple = {
+    name: 'apple',
+    'hello-bye': '👋',
+    0: 1,
+    ['hello-bye']: '👋',
+}
+
+// 속성, 데이터에 접근하기 위해서
+apple.name; //마침표 표기법 dot notaiton
+console.log(apple["hello-bye"]); //대괄호 표기법 bracket notation
+apple['name'];
+
+// 속성 추가 (동적)
+apple.emoji = '🍎';
+console.log(apple['emoji']); //🍎
+
+// 속성 삭제
+delete apple.emoji;
+console.log(apple); //{ '0': 1, name: 'apple', 'hello-bye': '👋' }
---
+++ 2.compute.js
@@ -0,0 +1,32 @@
+const obj = {
+    name: '엘리',
+    //key //value
+    age: 20,
+    job: 'lawyer',
+};
+// 코딩하는 시점에, 정적으로 접근이 확정됨
+obj.name;
+obj.age;
+
+// 동적으로 속성에 접근하고 싶을 때 대괄호 표기법 사용!
+function getValue(obj, key){
+    return obj[key];
+}
+
+console.log(getValue(obj, 'name'));
+
+// 객체에 추가
+function addKey(obj, key, value){
+    obj[key] = value;
+}
+
+// 삭제
+function deleteKey(obj, key){
+    delete obj[key];
+}
+
+addKey(obj, 'job', 'engineer');
+console.log(obj); //{ name: '엘리', age: 20, job: 'engineer' }
+
+deleteKey(obj, 'job');
+console.log(obj); //{ name: '엘리', age: 20 }
